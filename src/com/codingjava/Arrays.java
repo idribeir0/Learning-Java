446e943c3df6e3d3b102a394c47c55bf0d3abda4
Ione Ribeiro <idribeiro08@gmail.com>
2025-03-04 16:29:17 -0300

    creating array class

---
+++ src/com/codingjava/Arrays.java
@@ -0,0 +1,19 @@
+package com.codingjava;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        int[] numbers1 = {2, 3, 4,5,1,4};
+
+        System.out.println(java.util.Arrays.toString(numbers));
+        System.out.println(java.util.Arrays.toString(numbers1));
+
+    }
+
+}
+
+
+//in java an array is a data structure that stores a fixed-size collection of elements of the same type
