4caf0038cea88ee9fa66a0edc168a56f79b5ffb1
Ione Ribeiro <idribeiro08@gmail.com>
2025-03-04 09:19:58 -0300

    creating variable class

---
+++ src/com/codingjava/variables.java
@@ -0,0 +1,10 @@
+package com.codingjava;
+
+public class variables {
+    //we use variable to store temporally some values
+    public static void main(String[] args) {
+        int age = 30;
+        System.out.println(age);
+    }
+}
+
