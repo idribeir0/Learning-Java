210cc506a1430283d8eec09cce1853d86347aa14
Ione Ribeiro <idribeiro08@gmail.com>
2025-03-04 10:00:12 -0300

    creating the reference types class

---
+++ src/com/codingjava/reference_types.java
@@ -0,0 +1,13 @@
+package com.codingjava;
+
+import java.util.Date;
+
+public class reference_types {
+
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
+
+//reference types are object based, and store references to memory location, allowing for more flexibility and behavior (methods, attributes)
