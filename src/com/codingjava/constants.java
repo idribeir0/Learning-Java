3e2a2d55f3eb030c1d8ae43ef5598c71d3b7c262
Ione Ribeiro <idribeiro08@gmail.com>
2025-03-05 20:22:19 -0300

    creating constant class

---
+++ src/com/codingjava/constants.java
@@ -0,0 +1,10 @@
+package com.codingjava;
+
+public class constants {
+
+    public static void main(String[] args) {
+        final float PI = 3.14F;
+    }
+}
+
+//we use constants when we dont want to change the variable value, in this case we need to include "final" at the beginning of the variable
