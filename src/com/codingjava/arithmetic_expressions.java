5d343cc994e13cffcd5ab522d431546ad22cef6f
Ione Ribeiro <idribeiro08@gmail.com>
2025-03-05 21:08:05 -0300

    creating the arithmetic expressions class

---
+++ src/com/codingjava/arithmetic_expressions.java
@@ -0,0 +1,21 @@
+package com.codingjava;
+
+import java.sql.SQLOutput;
+
+public class arithmetic_expressions {
+
+    public static void main(String[] args) {
+        double result = (double)10 / (double)3;
+        System.out.println(result);
+
+        int x = 1;
+        int y = x++;
+        System.out.println(x);
+        System.out.println(y);
+
+        x += 2;
+        System.out.println(x);
+
+
+    }
+}
