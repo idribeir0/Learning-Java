9b9c95a2e18d7d9b61d0a933a34cbde01b0b05f0
Ione Ribeiro <idribeiro08@gmail.com>
2025-03-24 19:27:30 -0300

    including comparison operator and logical operator file

---
+++ src/com/codingjava/comparison_operator.java
@@ -0,0 +1,14 @@
+package com.codingjava;
+
+public class comparison_operator {
+
+    public static void main (String[] args) {
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x >= y);
+        System.out.println(x < y);
+
+    }
+}
---
+++ src/com/codingjava/logical_operators.java
@@ -0,0 +1,10 @@
+package com.codingjava;
+
+public class logical_operators {
+
+    public static void main(String[] args) {
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;
+        System.out.println(isWarm);
+    }
+}
